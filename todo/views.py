685c891939cdf3d237586edb26f70adda261e5f2
Sara Oliver <s.oliver1@icloud.com>
2020-04-29 12:19:13 +0000

    create new item

--- todo/views.py
+++ todo/views.py
@@ -18,7 +18,13 @@ def create_an_item(request):
             form.save()
             return redirect(get_todo_list)
 
-    else:
-        form = ItemForm()
+    def create_an_item(request):
+        if request.method == "POST":
+            new_item = Item()
+            new_item.name = request.POST.get('name')
+            new_item.done = 'done' in request.POST
+            new_item.save()
 
-    return render(request, "item_form.html", {'form': form})
+        return redirect(get_todo_list)
+
+    return render(request, "item_form.html")
