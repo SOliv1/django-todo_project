c371b2e1cbe8fff28a66383b0ca7d1004ebbf9c3
Sara Oliver <s.oliver1@icloud.com>
2020-05-05 11:11:17 +0000

    mplement environments specific values for the clear

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,9 +9,16 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
 import dj_database_url
+if os.path.exists('env.py'):
+    import env
+
+if os.environ.get('DEVELOPMENT'):
+    development = True
+else:
+    development = False
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,20 +28,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f0w+1d%a)o2_$(5p!t@xhmyy=&nubl*pvq(h^@bh%g42mk2^!0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('localhost', '127.0.0.1'),
-                 os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = ['127.0.0.1', 'sjo-django-todo.herokuapp.com',
+                 "HOST", 'localhost']
 
-
-host = os.environ.get('SITE_HOST')
-if host:
-    ALLOWED_HOSTS.append(host)
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -76,19 +77,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases#
 
-# DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#/}
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
+if "DATABASE_URL" in os.environ:
+    print("using postgres")
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
